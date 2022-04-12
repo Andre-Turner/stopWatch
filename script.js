6fdede70d373a431240cb773d3af92909337167b
DTurner94 <turnerdandre23@gmail.com>
2022-04-12 17:16:47 -0400

    projectComplete

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function resetTimer() {
     timeleft = -10;  
     updateTime();  
   }  
+
+  function stopTimer() {   
+    clearInterval(intervalVariable);  
+  }  
+  function updateTime() {  
+    timeleft = timeleft + 10;  
+    let timers = document.getElementById("timers");  
+    let timerms = document.getElementById("timerms");  
+    let milli = timeleft % 1000;  
+    timers.innerHTML = Math.floor(timeleft / 1000);  
+    timerms.innerHTML = Math.floor(milli / 10);  
+    timers.style.color = "green";  
+    timerms.style.color = "red";  
+  }  
